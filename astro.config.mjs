f2927b6f3898954ff95780b0e50e1c05d8e478cb
fessehaye <fessehay@ualberta.ca>
2026-04-07 15:20:35 -0700

    fix config for build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import markdoc from '@astrojs/markdoc';
-import keystatic from '@keystatic/astro';
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import markdoc from '@astrojs/markdoc'
+import keystatic from '@keystatic/astro'
+import tailwindcss from '@tailwindcss/vite'
+
+const SKIP_KEYSTATIC = process.env.NODE_ENV === 'production'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), markdoc(), keystatic()],
-    vite: {
-        plugins: [tailwindcss()],
-    },
-});
+  integrations: [react(), markdoc(), SKIP_KEYSTATIC ? null : keystatic()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})
